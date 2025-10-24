f9e7fadebacc1ed863bdda712533ec0cbdbc8b1a
sussybocca <babyyodacutefry@gmail.com>
2025-10-24 12:29:26 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,12 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5173
-  }
+  build: {
+    rollupOptions: {
+      // Ignore native modules that cause Netlify builds to fail
+      external: ["fsevents", "fs", "path", "os", "child_process"],
+    },
+  },
 });
